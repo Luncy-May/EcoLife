9ccd89c13df5b50a32d7e822e864bfb3266db1d0
Luncy-May <cheng.li1@wustl.edu>
2024-10-10 01:37:01 -0500

    update dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import ExpensePlan from './components/ExpensePlan'
-// npm install @mui/material react-markdown
+// npm install @mui/material @emotion/react @emotion/styled react-markdown
 function App() {
 
   return (
